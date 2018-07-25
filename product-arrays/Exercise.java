a1329d6cd895a59c6ac282a29b7b5aac23b2145e
Ironi Júnior Medina <ironimedina@gmail.com>
2018-07-25 14:31:29 -0300

    Added exercise product arrays

---
+++ product-arrays/Exercise.java
@@ -0,0 +1,20 @@
+public class Exercise {
+	public static void main(String[] args) {
+		List<Integer> input = Arrays.asList(1,2,3,4);
+		Integer[] outp = new Integer[input.size()];
+		for(int i = 0; i < input.size(); i++) {
+			outp[i] = multi(i, input);
+		}
+		System.out.println(Arrays.asList(outp));
+	}
+	
+	private static Integer multi(Integer ignore, List<Integer> input) {
+		Integer res = 0;
+		for(int j = 0; j < input.size(); j++) {
+			if(j != ignore) {
+				res = res * input.get(j);
+			}
+		}
+		return res;
+	}
+}
